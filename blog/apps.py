fbf020f809ba12320daebb3c994e6a06b2d7d338
parkhongbeen <pack122@naver.com>
2019-12-10 17:21:34 +0900

    add blog app

--- blog/apps.py
+++ blog/apps.py
@@ -3,13 +3,3 @@ from django.apps import AppConfig
 
 class BlogConfig(AppConfig):
     name = 'blog'
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'blog',
-]
